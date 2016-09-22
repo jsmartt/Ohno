cdd08245c6d6262bfba1c9a4f298ec627933854d
Jared Smartt <jared.smartt@hpe.com>
2016-09-22 16:58:18 -0600

    Added id_only option

--- lib/chef/knife/ohno.rb
+++ lib/chef/knife/ohno.rb
@@ -20,6 +20,13 @@ module Lnxchk
 
     banner "knife ohno HOURS"
 
+    option :id_only,
+      short: '-i',
+      long: '--id-only',
+      description: 'Show only matching object IDs',
+      boolean: true | false,
+      default: false
+
     def run
       unless hours = name_args.first
         ui.error "You need to specify a number of hours behind the checkins are"
@@ -35,9 +42,12 @@ module Lnxchk
 
       cutoff_time = Time.now - hours * 3600
       hitlist.delete_if { |node| Time.at(node['ohai_time']) > cutoff_time rescue false }
-      puts "\nLost cheep in need of chefherding for more than #{hours} hours:"
-      puts '  (None)' if hitlist.empty?
-      puts knife_status.ui.presenter.format(hitlist)
+      puts "#{hitlist.size} lost cheep in need of chefherding for more than #{hours} hours:\n\n"
+      if config[:id_only]
+        hitlist.each { |n| puts n['name'] }
+      else
+        puts knife_status.ui.presenter.format(hitlist)
+      end
     end # close run
   end # close class
 end # close module
